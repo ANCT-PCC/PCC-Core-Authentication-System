722baa68431c123b478a30d885500e2c7b5835c3
BATOER-0605 <nem0605@outlook.com>
2024-06-20 18:06:02 +0900

    test

--- resetup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker-compose stop
-docker rm pcc-cas-db
-docker rmi mysql
-rm -r mysql/
-
-docker-compose up -d
--- uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker stop pcc-cas
-docker rm pcc-cas
-docker rmi pcc-cas
-docker volume remove pcc-cas
